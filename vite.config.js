19bc7c93158ec59170792c7a8fbb92a59daffd30
FaramarzKowsari <105053743+FaramarzKowsari@users.noreply.github.com>
2026-02-11 04:00:49 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // اگر نام مخزن گیت‌هاب شما چیزی غیر از suya-nakis است، عبارت زیر را تغییر دهید
   base: '/suya-nakis/', 
 })
